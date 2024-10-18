98a39de020405a4f5b62f7fbfd8801de667708ef
A B M Asadullah <abmasadullah@gmail.com>
2024-10-18 12:20:49 +0600

    bg modify

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import CurrencyConverter from "./components/CurrencyConverter";
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <div className="main-container min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <div className="container">
         <CurrencyConverter />
       </div>
